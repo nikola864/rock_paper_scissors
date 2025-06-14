84df013680fc9e31bba268fb61b3913ac1617f1e
nikola864 <kolyab89217115399@mail.ru>
2025-06-15 00:37:20 +0300

    Add game logic

--- game.py
+++ game.py
@@ -1,7 +1,46 @@
 import random
 
-def play_game():
+def play_game(): # Основная функция
     print("Добро пожаловать в игру 'Камень-Ножницы-Бумага'")
     print("Правила игры: камень побеждает ножницы, ножницы побеждает бумагу, бумага побеждает камень")
 
-play_game()
+    options = ["камень","ножницы","бумага"] # список из которого компьютер делает выбор
+    while True:
+        # условия чтобы закончить игру
+        user_choice = input("Выберите камень,ножницы или бумагу(для оканчания игры напишите 'выход'):")
+        if user_choice == "выход":
+            print("Спасибо за игру! До новых встреч!!!")
+            break
+        # условие если юзер ввёл что-то другое(не камень, ножницы, бумага)
+        if user_choice not in options:
+            print("Некорректный ввод данных! Посмотрите правила игры и попробуйте ещё раз!")
+            continue
+
+        # чтоб ввод был красивым и понятным пользователю
+        comp_choice = random.choice(options)
+        print(f"Ваш выбор: {user_choice}")
+        print(f"Выбор_компьютера: {comp_choice}")
+
+        # условие если ничья
+        if comp_choice == user_choice:
+            print("Ничья")
+
+        # условия определения победителя
+        if user_choice == "камень" and comp_choice == "ножницы":
+            print("Ты победил")
+        if user_choice == "камень" and comp_choice == "бумага":
+            print("Компьютер победил")
+        if user_choice == "ножницы" and comp_choice == "камень":
+            print("Компьютер победил")
+        if user_choice == "ножницы" and comp_choice == "бумага":
+            print("Ты победил")
+        if user_choice == "бумага" and comp_choice == "камень":
+            print("Ты победил")
+        if user_choice == "бумага" and comp_choice == "ножницы":
+            print("Компьютер победил")
+
+
+
+
+if name == 'main':
+    play_game()
